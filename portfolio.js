9d532a6c8a45ebab99a40eb8ad7d4340ab9c59f5
ERika-tech257 <ErikaJtech@gmail.com>
2021-04-04 22:20:51 -0400

    css for contact and footer done. Media quesries for responsive design complete, added query for scroll function and added AOS for fading in different sections

--- portfolio.js
+++ portfolio.js
@@ -5,4 +5,29 @@
          $(this).toggleClass('open');
          $('.top-nav').toggleClass('open');
      });
+
+     $('.top-nav .nav-link').on('click', function () {
+         $('.menu-toggler').removeClass('open');
+         $('.top-nav').removeClass('open');
+     });
+
+     $('nav a[href*="#"]').on('click', function (){
+        $('html, body').animate({
+            scrollTop: $($(this).attribute('href')).offset().top - 100
+        }, 4000);
+    });
+
+    $('#up').on('click' , function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    // Animate webpage
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
  });
